190669d57f231d8a978dd2c1570c151524353c3c
Hugo Ruiz Sanchez <hugo.ruiz@amplia.es>
2024-07-16 18:46:34 +0200

    Eliminadas ";" innecesarias y adaptado el código para funcionar en Linux

--- codigo/interprete_htma.py
+++ codigo/interprete_htma.py
@@ -3,10 +3,10 @@
 # HTMA
 
 import os; 
-import re; 
-import markdown;
-input_path= "C:\\Users\\hrs\\Desktop\\autoweb\\web_prueba";
-output_path= "C:\\Users\\hrs\\Desktop\\autoweb\\salida_prueba";
+import re 
+import markdown
+input_path= "/home/hugo/eclipse-workspace/programacionPyton/autoweb/web_prueba"
+output_path= "/home/hugo/eclipse-workspace/programacionPyton/autoweb/salida_prueba"
 
 """
 El objetivo de la clase wblock es devolver los bloques HTMA correctamente formateados en formato
@@ -19,7 +19,7 @@ class WBlock ():
     # El processamiento de la etiqueta debe realizarse en un diccionario
     def __init__ (self, block):
         self.block = block
-        self.attributes = self.block_string_to_attributes (block);
+        self.attributes = self.block_string_to_attributes (block)
         
     # MÉTODOS
     
@@ -28,9 +28,9 @@ class WBlock ():
     """
     
     def generate_code (self):
-        output ="";
+        output =""
         if self.attributes["REDIR"] == "HTMA": 
-            files_to_redir = [link for link in self.list_directory_files() if ".htma" in link ]; 
+            files_to_redir = [link for link in self.list_directory_files() if ".htma" in link ] 
             for link in files_to_redir:
                 code= ""
                 
@@ -55,16 +55,16 @@ class WBlock ():
         for label in self.htm_ids(path):
             output = output.replace("$"+label+"$", self.htm_ids(path)[label]) 
         output = output.replace ("$DIR_LINKS$", self.obtener_nombre_archivo_desde_path(path).replace("htma", "html"))
-        self.attributes["FORMAT"] = output;
+        self.attributes["FORMAT"] = output
     
     """
     Acceder a un archivo HTMA/L y extraer las ID de sus etiquetas
     """
     
     def htm_ids (self, htm_path): 
-        output = {};
+        output = {}
         with open(htm_path, "r",  encoding="utf-8") as file:
-            htm = file.read().split(">");
+            htm = file.read().split(">")
                 
         for i in range(len(htm)):
             if 'id="' in htm[i] or "id='" in htm[i]:
@@ -74,8 +74,8 @@ class WBlock ():
                     matches = re.findall(r'src=("[^"]+"|\'[^\']+\')', htm[i])
                     output[htma_id.replace("'", "").replace('"', "")] = matches[0].replace("'", "").replace('"', "")
                 else:
-                    output[htma_id.replace("'", "").replace('"', "")] =  htm[i+1].split("<")[0].strip();
-        return output;
+                    output[htma_id.replace("'", "").replace('"', "")] =  htm[i+1].split("<")[0].strip()
+        return output
      
     """
     Lista los archivos de un directorio
@@ -83,16 +83,16 @@ class WBlock ():
     def list_directory_files (self):
         output = []
         if not self.attributes["DIR"]: 
-            return False; 
+            return False 
         
         if self.attributes["DIR"] == "$ROOT$":
-            self.attributes["DIR"]=input_path; 
+            self.attributes["DIR"]=input_path 
             
         for path in os.listdir(self.attributes["DIR"]):
             if os.path.isfile(os.path.join(self.attributes["DIR"], path)):
-                output.append(path);
+                output.append(path)
                 
-        return output; 
+        return output 
         
     """
     Procesa el bloque de texto y lo devuelve en forma de dicccionario de python, para acceder a las variables introducidas. 
@@ -100,24 +100,25 @@ class WBlock ():
         
     def block_string_to_attributes (self,block): 
 
-        attributes = {};   
+        attributes = {}   
 
         block = block.split (";")
 
-        clean_block =""; 
+        clean_block ="" 
         
         for trace in block:
             try: 
                 if "FORMAT" in trace: 
-                    trace = trace.split("{")[0].replace(" ", "")+ trace.split("{")[1].strip().replace("}", " ").strip(); 
+                    trace = trace.split("{")[0].replace(" ", "")+ trace.split("{")[1].strip().replace("}", " ").strip() 
                 else: 
                     trace = trace.split(":")[0].replace(" ", "")+":"+ trace.split(":")[1].strip()
             except:
                 pass # Omitir registros basura
                 
-            clean_block = clean_block + trace +";";
+            clean_block = clean_block + trace +";"
+
                 
-        block = clean_block.replace("[-HTMA=", "").replace("END-]", "").split(";");
+        block = clean_block.replace("[-HTMA=", "").replace("END-]", "").split(";") 
         block =  [trace for trace in block if trace.strip()]
 
         for trace in block: 
@@ -126,16 +127,16 @@ class WBlock ():
                 value= trace.split (":")[1]
                 attributes[key]=value; 
             except: 
-                print ("Formatting error at attribute at:", key, value);
+                print ("Formatting error at attribute at:", key, value)
             
         return attributes; 
         
     
 block = WBlock('DIR: $ROOT$; REDIR: HTMA; FORMAT: { <a href="$DIR_LINKS$"> <h3> $HTMA_TITLE$ </h3> <p> $HTMA_DESCRIPTION$ </p> </a> }')
-
-block.set_format_htm_ids(input_path+"\\articulos.htma")
-print (block.list_directory_files());
-print (block.attributes);
+print (input_path+"/articulos/articulos.htma")
+block.set_format_htm_ids(input_path+"/articulos/articulos.htma")
+print (block.list_directory_files())
+print (block.attributes)
 print (block.generate_code())
 
 """
